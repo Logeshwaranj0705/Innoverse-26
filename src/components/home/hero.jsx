02af5f9e9879421d07bd509dea6dccd5f2ced284
logesh_07_05 <naru8sasu9344@gmail.com>
2026-02-10 23:48:42 +0530

    Update hero.jsx

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -39,7 +39,7 @@ export default function Hero() {
             <motion.p
               className="mt-5 ml-2 tracking-[0.3em] text-sm text-gray-300"
             >
-              24-HOUR INTER-COLLEGE HACKATHON
+              24-HOUR NATIONAL-LEVEL HACKATHON
             </motion.p>
 
             <div className="mt-14">
